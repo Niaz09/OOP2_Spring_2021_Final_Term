05b4b4bdcdd3b846a0d63359bb0b3921fd5fcff1
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-03-22 09:53:30 +0600

    Account Class Final 01

--- Account_Class_Final_01/Account_Class_Final_01/Account.cs
+++ Account_Class_Final_01/Account_Class_Final_01/Account.cs
@@ -8,5 +8,74 @@ namespace Account_Class_Final_01
 {
     class Account
     {
+        
+        Transaction[] transactions;
+        private int totalNumberOfTransaction;
+
+        public string AccName { get; set; }
+        public string AccId { get; set; }
+        public double AccBalance { get; set; }
+
+        public Account() { }
+        public Account(string accName, string accId, double accBalance)
+        {
+            AccName = accName;
+            AccId = accId;
+            AccBalance = accBalance;
+            transactions = new Transaction[20];
+            totalNumberOfTransaction = 0;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + AccName);
+            Console.WriteLine("Acc No: " + AccId);
+            Console.WriteLine("Balance: " + AccBalance);
+        }
+        public void Deposit(double amount)
+        {
+            AccBalance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (AccBalance >= amount)
+            {
+                AccBalance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+
+        public void Transfer(Account a, double amount)
+        {
+            if (AccBalance >= amount)
+            {
+                AccBalance -= amount;
+                a.AccBalance += amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+        public void AddTransaction(params Transaction[] transactions)
+        {
+           
+        }
+        public void ShowAllTransaction()
+        {
+            for (int i = 0; i < totalNumberOfTransaction; i++)
+            {
+                transactions[i].ShowInfo();
+            }
+        }
+
+
     }
 }
+
+
+
